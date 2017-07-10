bfb8ede2d7a32ee4b596d14bcbbbf21424cdea82
pabfun7 <pabfun7@gmail.com>
2017-07-10 16:48:12 -0300

    Testing DB connection

--- src/test/java/TestDB.java
+++ src/test/java/TestDB.java
@@ -1,3 +1,6 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  * Created by pfunes on 10/07/17.
  */
@@ -6,6 +9,20 @@ public class TestDB {
 
     public static void main(String[] args) {
 
+        String jdbc = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+        String user = "springstudent";
+        String password = "springstudent";
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Connecting to: " + jdbc);
+            Connection myConn = DriverManager.getConnection(jdbc, user, password);
+            System.out.println("Connected!!");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
